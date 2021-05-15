1095830720b2d2e3962673b65845b8a0db9fd560
Ammon Johnson <ammonsawesome@gmail.com>
2021-05-15 10:09:41 -0700

    Added functions for the todo list

---
+++ js/main.js
@@ -0,0 +1,55 @@
+window.onload = function () {
+    var addItem = document.getElementById("add");
+    addItem.onclick = main;
+};
+function main() {
+    if (isValid()) {
+        var item_1 = getToDoItem();
+        displayToDoItem(item_1);
+    }
+}
+var ToDoItem = (function () {
+    function ToDoItem() {
+    }
+    return ToDoItem;
+}());
+var item = new ToDoItem();
+item.dueDate = new Date(2021, 6, 1);
+item.isCompleted = false;
+function isValid() {
+    return true;
+}
+function getToDoItem() {
+    var myItem = new ToDoItem();
+    var titleBox = getInput("title");
+    myItem.title = titleBox.value;
+    var dueDateInput = getInput("due-date");
+    myItem.dueDate = new Date(dueDateInput.value);
+    var isCompleted = getInput("complete");
+    myItem.isCompleted = isCompleted.checked;
+    return myItem;
+}
+function getInput(id) {
+    return document.getElementById(id);
+}
+function displayToDoItem(item) {
+    var itemText = document.createElement("h3");
+    itemText.innerText = item.title;
+    var itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toString();
+    var itemDiv = document.createElement("div");
+    itemDiv.classList.add("todo");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+    if (item.isCompleted) {
+        var completedTask = document.getElementById("complete-items");
+        completedTask.appendChild(itemDiv);
+    }
+    else {
+        var incompleteTasks = document.getElementById("incompleted-items");
+        incompleteTasks.appendChild(itemDiv);
+    }
+}
---
+++ ts/main.ts
@@ -0,0 +1,92 @@
+window.onload = function() {
+    let addItem = document.getElementById("add");
+    addItem.onclick = main;
+}
+
+function main() {
+    if(isValid()) {
+        let item = getToDoItem()
+        displayToDoItem(item);
+    }
+}
+
+class ToDoItem {
+    title:string;
+    dueDate:Date;
+    isCompleted:boolean;
+}
+
+let item = new ToDoItem();
+// item.title = "Testing";
+item.dueDate = new Date(2021, 6, 1);
+item.isCompleted = false;
+
+/**
+ * Check form data is valid
+ */
+function isValid():boolean {
+    return true;
+}
+
+/**
+ * Get all input off form and wrap in
+ * a ToDoItem
+ */
+function getToDoItem():ToDoItem {
+    let myItem = new ToDoItem();
+
+    // Get the title
+    let titleBox = getInput("title");
+    myItem.title = titleBox.value;
+
+    // Get the due date
+    let dueDateInput = getInput("due-date");
+    myItem.dueDate = new Date(dueDateInput.value);
+
+    // Get isCompleted 
+    let isCompleted = getInput("complete");
+    myItem.isCompleted = isCompleted.checked; 
+
+    return myItem;
+}
+
+function getInput(id):HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
+}
+
+/**
+ * Display given ToDoItem on the web page
+ * @param item ToDoItem
+ */
+function displayToDoItem(item:ToDoItem) {
+    // Create the title of the task
+    let itemText = document.createElement("h3");
+    itemText.innerText = item.title;
+
+    // Create the due date
+    let itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toString();
+
+    // Create a div for incomplete and completed tasks
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("todo");
+    if(item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+
+    // Add the the task to the completed div and incomplete div
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+
+    if(item.isCompleted) {
+        let completedTask = document.getElementById("complete-items");
+        completedTask.appendChild(itemDiv);
+    }
+
+    else {
+        let incompleteTasks = document.getElementById("incompleted-items");
+        incompleteTasks.appendChild(itemDiv);
+    }
+}
+
+// Allow user to mark a ToDoItem as completed
